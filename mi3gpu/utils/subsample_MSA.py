192f89c60f21f853c2ae6209625c506f2030d137
Allan Haldane <allan.haldane@gmail.com>
2020-06-01 19:49:21 -0400

    MAINT: update subseq_MSA to account for ids

--- mi3gpu/utils/subsample_MSA.py
+++ mi3gpu/utils/subsample_MSA.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python3
 import numpy as np
 from numpy.random import randint
-import sys, os
+import sys, os, argparse
 
 import mi3gpu.utils.seqload as seqload
 import mi3gpu.utils.seqtools as seqtools
 
-s = seqload.loadSeqs(sys.argv[1])[0]
-cutoff = 1-float(sys.argv[2])
-L = s.shape[1]
+parser = argparse.ArgumentParser(
+    description='remove sequences too similar to another sequence')
+parser.add_argument('seqs')
+parser.add_argument('cutoff', type=float)
+parser.add_argument('--ind', help='save indices')
+args = parser.parse_args()
 
-inds = []
+s, ids, _ = seqload.loadSeqs(args.seqs)
+cutoff = 1-float(args.cutoff)
+N, L = s.shape
+
+inds = None
+remaining_inds = None
+if args.ind is not None:
+    inds = []
+    remaining_inds = np.arange(N)
+
 out_seq = []
+out_ids = []
 while s.shape[0] != 0:
     ind = randint(s.shape[0])
+    keep = np.sum(s == s[ind,:], axis=1)/float(L) < cutoff
+
     out_seq.append(s[ind].copy()) # no ref to s
-    s = s[np.sum(s == s[ind,:], axis=1)/float(L) < cutoff,:]
+    s = s[keep,:]
+
+    if ids is not None:
+        out_ids.append(ids[ind])
+        ids = ids[keep]
+
+    if inds is not None:
+        inds.append(remaining_inds[ind])
+        remaining_inds = remaining_inds[keep]
     print(s.shape, file=sys.stderr)
 
 with os.fdopen(sys.stdout.fileno(), 'wb', closefd=False) as fp:
-    seqload.writeSeqs(fp, np.array(out_seq))
+    seqload.writeSeqs(fp, np.array(out_seq), ids=out_ids or None)
+
+if inds is not None:
+    np.save(args.ind, inds)
